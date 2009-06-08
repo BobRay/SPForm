734e2f9b47aa12854d0522818f2ba7ad9f8f3fba
splittingred <splittingred@24346d65-fb89-4855-a768-f5fb827ddd76>
2009-06-08 15:45:46 +0000

    [SPForm] - Removing no longer valid files
    git-svn-id: http://svn.modxcms.com/svn/modx-components/spform/trunk@463 24346d65-fb89-4855-a768-f5fb827ddd76

--- spform/js/usedkeyboard.js
+++
@@ -1,6 +0,0 @@
-var keysPressed = 0;
-document.onkeypress = logKeys;
-function logKeys() {
-	keysPressed++;
-	document.getElementById("keyCount").value = keysPressed;
-}
--- spform/templates/spfresponse.tpl
+++
@@ -1,9 +0,0 @@
-   <div id="spf_response">
-   <h3>[[+spf-thank-you]]</h3>
-
-   [[+spf-came-from]]
-
-   [[+spf-back]]
-
-   </div>
-
