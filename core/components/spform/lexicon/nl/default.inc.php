652f6078304b03b1b91728c3c2ade911ef5ce09f
bobray <bobray@24346d65-fb89-4855-a768-f5fb827ddd76>
2010-09-14 03:37:46 +0000

    Added Dutch Lexicon File
    git-svn-id: http://svn.modxcms.com/svn/modx-components/spform/trunk@867 24346d65-fb89-4855-a768-f5fb827ddd76

---
+++ core/components/spform/lexicon/nl/default.inc.php
@@ -0,0 +1,62 @@
+<?php
+/* Dutch language file for SPForm package */
+
+/* form field labels */
+$_lang['hidden-last-name'] = "Gelieve hier uw achternaam<br />niet in te vullen";
+$_lang['send-to'] = "Zenden naar";
+$_lang['your-name'] = "Uw naam";
+$_lang['email-address'] = "Email adres";
+$_lang['subject'] = "Onderwerp";
+$_lang['math-problem'] = "Los de rekenoefening op a.u.b.";
+$_lang['veri-string'] = "Gelieve de controletekst in te voeren.";
+$_lang['verification'] = "Controle";
+$_lang['enter-comments'] = "Vul uw reactie hieronder in.  Klik &quot;Verzenden&quot; wanneer u klaar bent.";
+$_lang['submit'] = "Verzenden";
+$_lang['reset'] = "Alles wissen";
+$_lang['wrote'] = "Schreef:\n\n";
+/* error messages */
+$_lang['bad-referer'] = "Fout: Verboden HTTP verwijzing!";
+$_lang['cookies-required'] = "Cookies moeten aan staan om dit formulier te gebruiken.";
+$_lang['form-errors'] = "Er waren problemen met het verzenden. Gelieve terug te keren en volgende fouten te verbeteren:";
+$_lang['could-not-initiate-mail-service'] = "Fout: Kon de MODx mailing dienst niet starten.";
+$_lang['unauthorized'] = "Fout: Geen Toegang";
+$_lang['no-captcha'] = "Fout: De Captcha plugin moet ge&iuml;nstalleerd zijn om Captche te gebruiken in SPForm.";
+$_lang['no-config'] = "Fout: Snippet eigenschappen niet ingesteld";
+$_lang['no-js'] = "Bestand niet gevonden: ";
+$_lang['no-template'] = "Kan de template chunk niet vinden: ";
+$_lang['set-email'] = "Gelieve uw e-mail adres in te vullen bij de &quot;emailsender&quot; on System Settings";
+
+/* email errors */
+$_lang['fatal1'] = "Er waren problemen met het verznden.";
+$_lang['fatal2'] = "Dit is mogelijk niet uw fout en is waarschijnlijk niet onmiddellijk op te lossen.<br />Gelieve later opnieuw te proberen.";
+$_lang['unauthorized-server'] = "Een niet toegelaten server probeerde toegang te krijgen tot dit formulier!";
+$_lang['banned'] = "Poging van een geblokkeerd e-mail adres, host of domein: ";
+$_lang['form-problem'] = "Probleem met het verwerken van het formulier";
+$_lang['form-problem-content'] = "De volgende problemen deden zich voor bij het verwerken van het contactformulier:";
+$_lang['bad-file'] = "Kan het databestand niet openen: ";
+$_lang['bad-destination'] = " is een ongeldige bestemming.";
+$_lang['no-email'] = "U hebt uw e-mail adres niet ingevuld.";
+$_lang['bad-email'] = "is geen geldig e-mail adres";
+$_lang['no-name'] = "U hebt uw naam niet ingevuld.";
+$_lang['no-subject'] = "U hebt geen onderwerp ingevuld.";
+$_lang['no-comments'] = "U hebt geen reactie niet ingevuld.";
+$_lang['mouse-kb-warning'] = "Geen muis en/of toetsenbord gebruikt";
+$_lang['mouse-warning'] = "De muis werd niet gebruikt";
+$_lang['kb-warning'] = "Het toetsenbord werd niet gebruikt";
+$_lang['last-name'] = "Vul het &quot;achternaam&quot; veld niet in";
+$_lang['timeout1'] = "Te weinig of te veel tijd verstreek bij het invullen van het formulier.";
+$_lang['timer-violation'] = "Timer overtreding";
+$_lang['no-time'] = "De timer staat aan maar er werd geen tijd ontvangen";
+$_lang['bad-verification'] = "Controleren van de controletekst mislukt.";
+$_lang['bad-recipient'] = "Ongeldige ontvanger: ";
+$_lang['bad-recipient-length'] = "Ontvanger veld te lang";
+$_lang['bad-subject-length'] = "Onderwerp veld te lang";
+$_lang['illegal-subject'] = "Ongeldige inhoud in onderwerp: ";
+$_lang['illegal-message'] = "Ongeldige inhoud in bericht: ";
+$_lang['content-links'] = "Te veel links in de inhoud: ";
+$_lang['mail-failure'] = "Sorry - Mailing systeem rapporteert fout bij het verzenden van het bericht<br />De meest waarschijnlijke oorzaak is een ongeldig terugzend e-mail adres.<br><br>Opmerking: als de mail() functie van de ISP geblokkeerd is zou de webmaster de SMTP optie moeten proberen";
+/* response messages */
+$_lang['thank-you'] = "Bedankt voor uw reactie!";
+$_lang['came-from'] = "We onthielden waar je was op de website.<br />Klik op de link hieronder op terug te keren.";
+$_lang['back'] = "Terugkeren";
+
