7e4c103fd7c494dbe68a61a80be65f07121b195f
bobray <bobray@softville.com>
2011-08-09 21:45:36 -0700

    refactored snippet property array

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -12,28 +12,28 @@
 
 $snippets = array();
 
-$snippets[1]= $modx->newObject('modSnippet');
-$snippets[1]->fromArray(array(
+$snippets[0]= $modx->newObject('modSnippet');
+$snippets[0]->fromArray(array(
     'id' => 1,
     'name' => 'SPForm',
     'description' => 'SPForm '.PKG_VERSION.'-'.PKG_RELEASE
             .' -  Creates a contact form for your site',
 ),'',true,true);
-$snippets[1]->setContent(file_get_contents($sources['source_core'] . '/spform.inc.php'));
+$snippets[0]->setContent(file_get_contents($sources['source_core'] . '/spform.inc.php'));
 $properties = include $sources['data'].'properties.spform.php';
-$snippets[1]->setProperties($properties);
+$snippets[0]->setProperties($properties);
 unset($properties);
 
 
-$snippets[2]= $modx->newObject('modSnippet');
-$snippets[2]->fromArray(array(
+$snippets[1]= $modx->newObject('modSnippet');
+$snippets[1]->fromArray(array(
     'id' => 2,
     'name' => 'SPFResponse',
     'description' => 'SPForm "Thank You" page',
 ),'',true,true);
-$snippets[2]->setContent(file_get_contents($sources['source_core'] . '/spfresponse.inc.php'));
+$snippets[1]->setContent(file_get_contents($sources['source_core'] . '/spfresponse.inc.php'));
 $properties = include $sources['data'].'properties.spfresponse.php';
-$snippets[2]->setProperties($properties);
+$snippets[1]->setProperties($properties);
 unset($properties);
 
 return $snippets;
