e6e3006cdb72f8d11c2b5d8f97ec16a326323ea6
bobray <bobray@softville.com>
2023-01-10 15:16:36 -0600

    Mods for MODX 3; make sure we do not trash existing resources

--- _build/install.script.php
+++ _build/install.script.php
@@ -29,27 +29,78 @@
  *
  * @var $object modX
  * @var $options array
- * @var $resource modResource
+ * @var $modx modx
  */
 
 $success = false;
-/** @var */
-$object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Running PHP Resolver.');
+$modx =& $object->xpdo;
+
+$prefix = $modx->getVersionData()['version'] >= 3
+    ? 'MODX\Revolution\\'
+    : '';
+
+$modx->log(xPDO::LOG_LEVEL_INFO,'Running PHP Resolver.');
 switch($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_INSTALL:
         /* get thank you resource */
-        $resource = $object->xpdo->getObject('modResource',array(
-            'pagetitle' => 'Thank You',
+        /** @var modResource $thanksPage */
+        $thanksPage = $modx->getObject($prefix . 'modResource',array(
+            'alias' => 'thankyou',
         ));
-        if ($resource == null) {
-            $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not find Thank You resource.');
+        if ($thanksPage == null) {
+            $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not find resource with alias "thankyou".');
+            return false;
+        }
+        /** @var string $c */
+        $c = $thanksPage->getContent();
+
+        if (strpos($c, '[[!SPFResponse]]') === false) {
+            /* not our thank-you page */
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'Site already has a resource with alias "thankyou". Please change its alias and try again.');
             return false;
         }
 
-        $temp_spfresponse_id = (integer) $resource->get('id');
-        $myServer = $_SERVER['HTTP_HOST'];
+        /** @var modResource $contactPage */
+        $contactPage = $modx->getObject($prefix . 'modResource', array('alias' => 'contact'));
 
-        $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Setting recipientArray.');
+        if (!$contactPage) {
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'Could not get "Contact" Resource');
+             return false;
+        }
+
+        $c = $contactPage->getContent();
+
+        if (strpos($c, '[[!SPForm]]') === false) {
+            /* not our contact page */
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'Site already has a resource with alias "contact". Please change its alias and try again.');
+            return false;
+        }
+        $temp_spform_id = (integer)$contactPage->id; /* used below to set the Thank-You page parent */
+        $temp_spfresponse_id = (integer) $thanksPage->get('id');
+        $myServer = $_SERVER['HTTP_HOST'];
+        $default_template = $modx->getOption('default_template');
+
+        $modx->log(xPDO::LOG_LEVEL_INFO, 'Setting Contact and Thank You pages to default template: ' . $default_template);
+
+        /* store ID of contact page for uninstall */
+        $fp = fopen(MODX_CORE_PATH . 'components/spform/contactid.txt', 'w');
+        fwrite($fp, $temp_spform_id);
+        fclose($fp);
+        $contactPage->set('template', $default_template);  /* give it the default template */
+        $contactPage->set('isfolder', 1);
+        if ($contactPage->save() === false) {
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'Could not save Contact page');
+        }
+
+        /*  Now the "Thank You" page  */
+        $thanksPage->set('template', $default_template);  /* give it the default template */
+        $thanksPage->set('parent', $temp_spform_id);   /* set parent to contact page */
+        if ($thanksPage->save() === false) {
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'Could not save Thanks page');
+        }
+
+        /* Set recipient array in snippet properties */
+        $modx->log(xPDO::LOG_LEVEL_INFO,'Setting recipientArray.');
 
         if (strstr($myServer,'www')) {
             $temp = str_replace('www.','',$myServer);
@@ -59,24 +110,20 @@ switch($options[xPDOTransport::PACKAGE_ACTION]) {
         }
 
         /* get snippet */
-        /** @var $obj modSnippet */
-        $obj = $object->xpdo->getObject('modSnippet',array(
+        /** @var $snippet modSnippet */
+        $snippet = $modx->getObject($prefix . 'modSnippet',array(
             'name'=>'SPForm'
         ));
-        if (!$obj) {
-          $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not get SPForm object');
+        if (!$snippet) {
+          $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not get SPForm snippet object');
         } else {
             $newRecipientArray = 'Webmaster :' . $options['user_email'];
-            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Setting recipientArray: ' . $newRecipientArray);
-            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Setting errorsTo: ' . $options['user_email'] );
-            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Setting spfResponseID: ' . $temp_spfresponse_id);
-            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Setting formProcAllowedReferers: ' . $myServer);
-
-
-
+            $modx->log(xPDO::LOG_LEVEL_INFO,'Setting recipientArray: ' . $newRecipientArray);
+            $modx->log(xPDO::LOG_LEVEL_INFO,'Setting errorsTo: ' . $options['user_email'] );
+            $modx->log(xPDO::LOG_LEVEL_INFO,'Setting spfResponseID: ' . $temp_spfresponse_id);
+            $modx->log(xPDO::LOG_LEVEL_INFO,'Setting formProcAllowedReferers: ' . $myServer);
 
             $props = array(
-
                 array (
                     'name'=>'recipientArray',
                     'desc'=>'spf_recipientarray_desc',
@@ -85,8 +132,6 @@ switch($options[xPDOTransport::PACKAGE_ACTION]) {
                     'lexicon'=>'spform:properties',
                     'value'=>$newRecipientArray
                 ),
-
-
                 array(
                     'name'=>'errorsTo',
                     'desc'=>'spf_errorsto_desc',
@@ -110,104 +155,65 @@ switch($options[xPDOTransport::PACKAGE_ACTION]) {
                     'options'=>'',
                     'lexicon'=>'spform:properties',
                     'value'=>$myServer
-
-
                 )
-
             );
 
-
-             if ($obj->setProperties($props, true) == false) {
-                 $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not set properties of SPForm object');
+             if ($snippet->setProperties($props, true) == false) {
+                 $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not set properties of SPForm object');
              }
 
-
-            if ($obj->save() == false ) {
-                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not save SPForm properties');
+            if ($snippet->save() === false ) {
+                $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not save SPForm properties');
             }
         }
 
-        /* This just loads and saves the SPFResponse snippet unchanged so it will appear last in the tree */
-        $obj = $object->xpdo->getObject('modSnippet',array('name'=>'SPFResponse'));
+        /* This just loads and saves the SPFResponse snippet unchanged, so it will appear last in the tree */
+        $obj = $modx->getObject($prefix . 'modSnippet',array('name'=>'SPFResponse'));
         if (!$obj) {
-          $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not get SPFResponse object');
+          $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not get SPFResponse object');
         } else {
 
             if ($obj->save() == false ) {
-                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not save SPForm properties');
+                $modx->log(xPDO::LOG_LEVEL_ERROR,'Could not save SPForm properties');
             }
         }
-
-        /* Give the two resources the default template  */
-
-
-        $default_template = $object->xpdo->getOption('default_template');
-
-        $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Setting Contact and Thank You pages to default template: ' . $default_template);
-
-        /* First the Contact page  */
-        $obj = $object->xpdo->getObject('modResource',array('pagetitle'=>'Contact'));
-        $temp_spform_id = (integer) $obj->id; /* used below to set the Thank You page parent */
-        if (!$obj) {
-          $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not get "Contact" Resource');
-        } else {
-            /* store ID of contact page for uninstall */
-            $fp = fopen(MODX_CORE_PATH . 'components/spform/contactid.txt','w');
-            fwrite($fp,$obj->get('id'));
-            fclose($fp);
-            $obj->set('template',$default_template);  /* give it the default template */
-            $obj->set('isfolder',1);
-            if ($obj->save() == false ) {
-                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not save SPForm properties');
-            }
-        }
-        /*  Now the "Thank You" page  */
-        $obj = $object->xpdo->getObject('modResource',array('pagetitle'=>'Thank You'));
-        if (!$obj) {
-          $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not get "Thank You" Resource');
-        } else {
-            $obj->set('template',$default_template);  /* give it the default template */
-            $obj->set('parent',$temp_spform_id);   /* set parent to contact page */
-            if ($obj->save() == false ) {
-                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'Could not save SPFResponse properties');
-            }
-        }
-
         $success =  true;
         break;
+
     case xPDOTransport::ACTION_UPGRADE:
         $fName = MODX_CORE_PATH . 'components/spform/banlist.inc.php';
-        $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Upgrading SPForm.');
+        $modx->log(xPDO::LOG_LEVEL_INFO,'Upgrading SPForm.');
         if (file_exists($fName)) {
             $c = file_get_contents($fName);
-            $chunkObj = $object->xpdo->getObject('modChunk', array('name'=>'spfBanlist'));
+            $chunkObj = $modx->getObject($prefix . 'modChunk', array('name'=>'spfBanlist'));
             if ($chunkObj && ! empty($c)) {
-                $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Moving the content of your Banlist file into the new spfBanlist chunk.');
+                $modx->log(xPDO::LOG_LEVEL_INFO,'Moving the content of your Banlist file into the new spfBanlist chunk.');
                 $chunkObj->setContent($c);
                 if ($chunkObj->save()) {
                      if (rename ($fName,$fName . 'bak'))
-                         $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Renamed banlist file banlist.bak.');;
+                         $modx->log(xPDO::LOG_LEVEL_INFO,'Renamed banlist file banlist.bak.');;
                 }
             }
         }
 
         $success = true;
         break;
+
     case xPDOTransport::ACTION_UNINSTALL:
         /** @var $obj modResource
          *  @var $child modResource
          * */
-        $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Uninstalling . . .');
+        $modx->log(xPDO::LOG_LEVEL_INFO,'Uninstalling . . .');
         $fp = fopen(MODX_CORE_PATH . 'components/spform/contactid.txt','r');
         if ($fp) {
             $id = fread($fp,20);
             $id = (int) $id;
-            $obj = $object->xpdo->getObject('modResource',$id);
+            $obj = $modx->getObject($prefix . 'modResource',$id);
 
             fclose($fp);
         }
         if ($obj) {
-            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Removing "Contact" and "Thank You" resources.');
+            $modx->log(xPDO::LOG_LEVEL_INFO,'Removing "Contact" and "Thank You" resources.');
             $children = $obj->getMany('Children');
             $obj->remove();
             if (!empty ($children)) {
@@ -216,11 +222,11 @@ switch($options[xPDOTransport::PACKAGE_ACTION]) {
                 }
             }
         } else {
-            $object->xpdo->log(xPDO::LOG_LEVEL_WARN,'Note: You may have to remove the Contact and Thank You resources manually.');
+            $modx->log(xPDO::LOG_LEVEL_WARN,'Note: You may have to remove the Contact and Thank You resources manually.');
         }
         $success = true;
         break;
 
 }
-$object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Script resolver actions completed');
+$modx->log(xPDO::LOG_LEVEL_INFO,'Script resolver actions completed');
 return $success;
